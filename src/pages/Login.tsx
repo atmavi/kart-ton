0d41f897cfa6e70e8a2efb05c62aa53836f4d1ec
atmavi <atmavi0001@gmail.com>
2020-06-03 17:17:59 +0800

    Added username input validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,20 +6,44 @@ import {
    IonInput,
    IonButton
 } from '@ionic/react';
-import React from 'react';
+
+import React, { useState } from 'react';
+
 import './Login.css';
 
 const Login: React.FC = () => {
+   const [username, setUsername] = useState<any>('');
+   const [password, setPassword] = useState('');
+   const [usernameAlert, setUsernameAlert] = useState('');
+
+   function validateUsername() {
+      if (username.length < 6) {
+         setUsernameAlert('Username should at least be 6 characters.');
+      } else {
+         setUsernameAlert('');
+      }
+   }
+
+   function handleSubmit() {
+
+   }
+
+
    return (
       <IonPage>
 
          <IonContent className="ion-padding">
 
             <div className="logo ion-text-center logo">Kart-Ton</div>
-            <form action="">
+
+            <form action="/home">
                <IonItem class="ion-margin-top" >
                   <IonLabel position="stacked">Username</IonLabel>
-                  <IonInput></IonInput>
+                  <small>{usernameAlert}</small>
+                  <IonInput
+                     onIonChange={e => setUsername(e.detail.value)}
+                     onIonBlur={validateUsername}
+                  ></IonInput>
                </IonItem>
 
                <IonItem class="ion-margin-top">
@@ -27,9 +51,8 @@ const Login: React.FC = () => {
                   <IonInput type="password"></IonInput>
                </IonItem>
 
-               <a href="/home">
-                  <IonButton expand="block" className="login-btn">Login</IonButton>
-               </a>
+               <IonButton expand="block" className="login-btn">Login</IonButton>
+
             </form>
          </IonContent>
       </IonPage >
