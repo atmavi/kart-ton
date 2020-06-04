387179a7c19f1137ef9672357f7d0bbc1b12f975
atmavi <atmavi0001@gmail.com>
2020-06-04 10:38:04 +0800

    Initialized Projects page

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,21 @@
+import {
+   IonContent,
+   IonPage,
+   IonItem,
+   IonLabel,
+   IonInput,
+   IonButton
+} from '@ionic/react';
+import React, { useState } from 'react';
+import './Projects.css';
+
+const Projects: React.FC = () => {
+   return (
+      <IonPage>
+         <IonContent className="ion-padding">
+         </IonContent>
+      </IonPage>
+   );
+};
+
+export default Projects;
