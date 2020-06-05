177854fd0842950913067a579cbaf86485aaef81
atmavi <atmavi0001@gmail.com>
2020-06-05 16:01:11 +0800

    Initialize ViewProject component

---
+++ src/components/ViewProject/ViewProject.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+   IonItem,
+   IonLabel,
+   IonInput,
+   IonButton,
+   IonTextarea,
+   IonItemGroup,
+   IonItemDivider
+} from '@ionic/react';
+import './ViewProject.css';
+
+const ViewProject: React.FC = () => {
+   return (
+      <div className="container">
+         <h1>test</h1>
+      </div>
+
+   );
+};
+
+export default ViewProject;
