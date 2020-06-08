9ec06709f8d09977c25870e3ed2a5a861bc287b8
atmavi <atmavi0001@gmail.com>
2020-06-08 14:26:34 +0800

    Stored all avatars in const avatars to lessen code

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -25,38 +25,32 @@ export interface IProject {
 
 
 const Projects: React.FC = () => {
+   const avatars = [
+      'https://www.photosforclass.com/download/pixabay-310819?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e1d54b4b5bb108f5d084609620367d1c3ed9e04e507440742d7fd5934fc3_1280.png&user=Clker-Free-Vector-Images',
+      'https://pixabay.com/get/57e5d3464256b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png',
+      'https://pixabay.com/get/55e1d54b4a55b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png'
+   ];
+
    const [projects, setProjects] = useState<IProject[]>(
       [
          {
             title: 'Project 1',
             start: 'Jun 2020',
             end: 'Dec 2020',
-            avatars: [
-               'https://www.photosforclass.com/download/pixabay-310819?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e1d54b4b5bb108f5d084609620367d1c3ed9e04e507440742d7fd5934fc3_1280.png&user=Clker-Free-Vector-Images',
-               'https://pixabay.com/get/57e5d3464256b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png',
-               'https://pixabay.com/get/55e1d54b4a55b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png'
-            ],
+            avatars: avatars,
             budget: 2000000
          },
          {
             title: 'Project 2',
             start: 'Jan 2020',
             end: 'Sep 2021',
-            avatars: [
-               'https://www.photosforclass.com/download/pixabay-310819?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e1d54b4b5bb108f5d084609620367d1c3ed9e04e507440742d7fd5934fc3_1280.png&user=Clker-Free-Vector-Images',
-               'https://pixabay.com/get/57e5d3464256b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png',
-               'https://pixabay.com/get/55e1d54b4a55b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png'
-            ]
+            avatars: avatars
          },
          {
             title: 'Project 3',
             start: 'Feb 1990',
             end: 'Nov 2020',
-            avatars: [
-               'https://www.photosforclass.com/download/pixabay-310819?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F55e1d54b4b5bb108f5d084609620367d1c3ed9e04e507440742d7fd5934fc3_1280.png&user=Clker-Free-Vector-Images',
-               'https://pixabay.com/get/57e5d3464256b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png',
-               'https://pixabay.com/get/55e1d54b4a55b10ff3d8992ccf2934771438dbf852547848772b7ad0954b_340.png'
-            ]
+            avatars: avatars
          }
       ]
    );
