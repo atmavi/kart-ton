4065adcbe7023f84cc143ce2dd3c1a6df232bac6
atmavi <atmavi0001@gmail.com>
2020-06-08 19:02:56 +0800

    Initialized ProjectFullDetails component

---
+++ src/components/ProjectFullDetails/ProjectFullDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { } from '@ionic/react';
+
+import './Project.css';
+
+
+const ProjectFullDetails: React.FC = () => {
+   return (
+      <h1>test</h1>
+   );
+};
+
+export default ProjectFullDetails;
