7d37e4efca480bfdfdaa601c68132ef6b29b2e4e
atmavi <atmavi0001@gmail.com>
2020-06-09 17:33:06 +0800

    Created interface for ProjectFullDetails prop

--- src/components/ProjectFullDetails/ProjectFullDetails.tsx
+++ src/components/ProjectFullDetails/ProjectFullDetails.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { } from '@ionic/react';
 
-import './Project.css';
+import './ProjectFullDetails.css';
+import { IProject, IViewedProject } from '../../pages/Projects/Projects';
 
+interface IFullProject {
+   viewedProject: IViewedProject
+}
 
-const ProjectFullDetails: React.FC = () => {
+const ProjectFullDetails: React.FC<IFullProject> = props => {
+   console.log(props);
    return (
+      // <h1 className={`${props.isOpen && 'ion-hide'} container`}>test</h1>
       <h1>test</h1>
    );
 };
